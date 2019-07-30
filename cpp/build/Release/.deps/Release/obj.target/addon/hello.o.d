58c7d2b9addc124a16f0ac2143a53f1f3dbe88c6
Manas <prometheansacrifice@gmail.com>
2019-07-30 21:24:43 +0530

    wip

---
+++ cpp/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,11 @@
+cmd_Release/obj.target/addon/hello.o := c++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/manas/Library/Caches/node-gyp/12.1.0/include/node -I/Users/manas/Library/Caches/node-gyp/12.1.0/src -I/Users/manas/Library/Caches/node-gyp/12.1.0/deps/openssl/config -I/Users/manas/Library/Caches/node-gyp/12.1.0/deps/openssl/openssl/include -I/Users/manas/Library/Caches/node-gyp/12.1.0/deps/uv/include -I/Users/manas/Library/Caches/node-gyp/12.1.0/deps/zlib -I/Users/manas/Library/Caches/node-gyp/12.1.0/deps/v8/include  -Os -gdwarf-2 -fvisibility=hidden -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c -o Release/obj.target/addon/hello.o ../hello.cc
+Release/obj.target/addon/hello.o: ../hello.cc \
+  /Users/manas/Library/Caches/node-gyp/12.1.0/include/node/node_api.h \
+  /Users/manas/Library/Caches/node-gyp/12.1.0/include/node/js_native_api.h \
+  /Users/manas/Library/Caches/node-gyp/12.1.0/include/node/js_native_api_types.h \
+  /Users/manas/Library/Caches/node-gyp/12.1.0/include/node/node_api_types.h
+../hello.cc:
+/Users/manas/Library/Caches/node-gyp/12.1.0/include/node/node_api.h:
+/Users/manas/Library/Caches/node-gyp/12.1.0/include/node/js_native_api.h:
+/Users/manas/Library/Caches/node-gyp/12.1.0/include/node/js_native_api_types.h:
+/Users/manas/Library/Caches/node-gyp/12.1.0/include/node/node_api_types.h:
